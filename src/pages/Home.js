6e9bd6fa88937bd6a1926c0c8f3753251cad83fa
praveenraajm <praveenraajm@outlook.com>
2024-01-31 21:50:45 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        Welcome to the world of photography
+      </h1>
+      <h3>Tour on Frames Pro</h3>
+    </div>
+  );
+};
+
+export default Home;
