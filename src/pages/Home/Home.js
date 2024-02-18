4c69581aeada137c98ff4ea3c561ef023faf7ae8
praveenraajm <praveenraajm@outlook.com>
2024-02-18 21:13:14 +0530

    #route issue after deployment fixed and styles changed as per mobile view

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,14 @@ const Home = () => {
       <div className="header-container">
         <h1>Welcome To Frames Pro</h1>
         <h2>Perception redefines the view!</h2>
+        <h4>
+          In the realm of my lens, I believe that perception is the key that
+          unlocks the boundless beauty hidden in every frame. "Perception
+          redefines the view" is not just a statement; it's a guiding principle
+          that propels me to explore the world with fresh eyes, to see beyond
+          the obvious, and to capture moments that challenge and reshape
+          conventional perspectives
+        </h4>
       </div>
       <div className="cover-image-container">
         <img src={coverImage} alt="cover-image" />
