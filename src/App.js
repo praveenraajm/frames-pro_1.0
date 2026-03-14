7d5904517245eed5e41084ab31b3fb831045af8f
praveenraajm <praveenraajm@outlook.com>
2026-03-14 20:52:05 +0530

    minor changes on removal of imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import NavBar from "./pages/NavBar/NavBar";
--- src/components/ImageLightbox.js
+++ src/components/ImageLightbox.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 
